b36e0cac6c575116598ac3e7724203045ee13bb6
Injoker24 <mcl.michaellee@gmail.com>
2023-06-23 23:17:11 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
+}
+
+let links = Array.from(document.querySelectorAll("a"));
+
+links.forEach(link => {
+    link.addEventListener("mouseover", () => {
+        innerCursor.classList.add("grow");
+    })
+
+    link.addEventListener("mouseleave", () => {
+        innerCursor.classList.remove("grow");
+    })
+})
+
+const observer1 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('project-header-animation');
+            return;
+        }
+    });
+});
+
+
+observer1.observe(document.querySelector('.project-header'));
