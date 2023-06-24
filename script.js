ad10832f61f078c49756a3994a2544034910f8ac
Injoker24 <mcl.michaellee@gmail.com>
2023-06-24 17:25:56 +0700

    Third Commit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ window.addEventListener("load", () => {
     function sendData(formProps) {   
         const username = formProps.username;
         const password = formProps.password;
+        document.querySelector(".error-username").innerHTML = "";
+        document.querySelector(".error-password").innerHTML = "";
         (async () => {
             const response = await fetch('https://enrichment-socs.apps.binus.ac.id/lp-api/api/auth/login', {
                 method: 'POST',
@@ -93,7 +95,6 @@ window.addEventListener("load", () => {
         
             const data = await response.json()
             lbData = data;
-            console.log(lbData);
             if(data.status == 200){
                 
             }
@@ -107,24 +108,64 @@ window.addEventListener("load", () => {
             const clockout = formProps.clockout;
             const activity = formProps.activity;
             const description = formProps.description;
-            (async () => {
-                const response = await fetch('', {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        clock_in: clockin,
-                        clock_out: clockout,
-                        activity: activity,
-                        description: description
-                    }),
-                    headers: {
-                        'Content-type': 'application/json; charset=UTF-8',
-                        'Authorization': token
-                    },
-                })
-            
-                const data = await response.json()
-                console.log(data);
-            })();
+            document.querySelector(".error-date").innerHTML = "";
+            document.querySelector(".error-activity").innerHTML = "";
+            document.querySelector(".error-clockin").innerHTML = "";
+            document.querySelector(".error-clockout").innerHTML = "";
+            document.querySelector(".error-description").innerHTML = "";
+            document.querySelector(".success-edit").innerHTML = "";
+            var found = false;
+            lbData.data.logbookMonth.map((data) => {
+                if(month == data.month) {
+                    data.log_book_month_details.map((data2) => {
+                        if(data2.date_filled == logbookdate) {
+                            found = true;
+                            (async () => {
+                                const response = await fetch('https://enrichment-socs.apps.binus.ac.id/lp-api/api/student/log-book/' + data2.uid + '/edit', {
+                                    method: 'POST',
+                                    body: JSON.stringify({
+                                        clock_in: clockin,
+                                        clock_out: clockout,
+                                        activity: activity,
+                                        description: description
+                                    }),
+                                    headers: {
+                                        'Content-type': 'application/json; charset=UTF-8',
+                                        'Authorization': token
+                                    },
+                                })
+                            
+                                const data = await response.json()
+                                console.log(data);
+
+                                if(data.status != 204 && data.message.activity){
+                                    document.querySelector(".error-activity").innerHTML = data.message.activity;
+                                }
+                    
+                                if(data.status != 204 && data.message.clock_in){
+                                    document.querySelector(".error-clockin").innerHTML = data.message.clock_in;
+                                }
+                    
+                                if(data.status != 204 && data.message.clock_out){
+                                    document.querySelector(".error-clockout").innerHTML = data.message.clock_out;
+                                }
+
+                                if(data.status != 204 && data.message.description){
+                                    document.querySelector(".error-description").innerHTML = data.message.description;
+                                }
+                                
+                                if(data.status == 204){
+                                    document.querySelector(".success-edit").innerHTML = "Berhasil mengedit logbook";
+                                }
+                            })();
+                        }
+                    });
+                }
+            });
+
+            if(found == false) {
+                document.querySelector(".error-date").innerHTML = "Date not found";
+            }
         }   
 
         const lbform = document.getElementById("logbookForm");
@@ -138,3 +179,8 @@ window.addEventListener("load", () => {
         }
     }
 });
+
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight) + "px";
+  }
